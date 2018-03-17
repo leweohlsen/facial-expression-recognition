05b4af51ff1b30def962859ad3c3a049674842a7
Lewe Ohlsen <mail@lewe.io>
2018-03-17 18:09:01 +0100

    add new files

---
+++ notebooks/plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as pyplot
+
+def plot_images(images, cls_true, img_size, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+    
+    # Create figure with 3x3 sub-plots.
+    fig, axes = pyplot.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i].reshape((img_size, img_size)), cmap='gray')
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        # Show the classes as the label on the x-axis.
+        ax.set_xlabel(xlabel)
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    pyplot.show()
