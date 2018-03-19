b3eb1099c31fee387fca0978377e811486100b87
Lewe Ohlsen <mail@lewe.io>
2018-03-19 23:02:55 +0100

    vgg-like architecture

--- model.py
+++ model.py
@@ -12,46 +12,47 @@ def conv_net(x_dict, n_classes, img_size, dropout, reuse, is_training):
 
         # The data input is a 1-D vector of 2304 features (48*48 pixels)
         # Reshape to match picture format [Height x Width x Channel]
-        # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+        # 4D Input-Tensor: [Batch Size, Height, Width, Channel]
         x = tf.reshape(x, shape=[-1, img_size, img_size, 1])
 
         tf.summary.image("img", x)
 
-        # Convolution Layer with 32 filters and a kernel size of 5
-        conv1 = tf.layers.conv2d(x, 32, 5, padding='same', activation=tf.nn.relu)
+        # Convolution Layers with 32 filters and a kernel size of 3.
+        conv1 = tf.layers.conv2d(x, 32, 3, padding='same', activation=tf.nn.relu)
+        conv1 = tf.layers.conv2d(conv1, 32, 3, padding='same', activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
-        # Convolution Layer with 64 filters and a kernel size of 3
+        # Convolution Layers with 64 filters and a kernel size of 3.
         conv2 = tf.layers.conv2d(conv1, 64, 3, padding='same', activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv2 = tf.layers.conv2d(conv2, 64, 3, padding='same', activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2.
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
         
         # Convolution Layer with 128 filters and a kernel size of 3
         conv3 = tf.layers.conv2d(conv2, 128, 3, padding='same', activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv3 = tf.layers.conv2d(conv3, 128, 3, padding='same', activation=tf.nn.relu)
+        conv3 = tf.layers.conv2d(conv3, 128, 3, padding='same', activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2.
         conv3 = tf.layers.max_pooling2d(conv3, 2, 2)
 
-        # Convolution Layer with 256 filters and a kernel size of 3
+        # Convolution Layers with 256 filters and a kernel size of 3.
         conv4 = tf.layers.conv2d(conv3, 256, 3, padding='same', activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv4 = tf.layers.conv2d(conv4, 256, 3, padding='same', activation=tf.nn.relu)
+        conv4 = tf.layers.conv2d(conv4, 256, 3, padding='same', activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2.
         conv4 = tf.layers.max_pooling2d(conv4, 2, 2)
 
-        # Convolution Layer with 256 filters and a kernel size of 3
-        conv5 = tf.layers.conv2d(conv4, 256, 3, padding='same', activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv5 = tf.layers.max_pooling2d(conv5, 2, 2)
+        # Flatten the data to a 1-D vector for the fully connected layer.
+        fc = tf.contrib.layers.flatten(conv4)
 
-        # Flatten the data to a 1-D vector for the fully connected layer
-        fc1 = tf.contrib.layers.flatten(conv4)
+        # Fully connected layer.
+        fc = tf.layers.dense(fc, 1024)
+        # Apply Dropout (only when is_training is True).
+        fc = tf.layers.dropout(fc, rate=dropout, training=is_training)
 
-        # Fully connected layer (in tf contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 1024)
-        # Apply Dropout (if is_training is False, dropout is not applied)
-        fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
-
-        # Output layer, class prediction
-        out = tf.layers.dense(fc1, n_classes)
+        # Output layer, class prediction.
+        out = tf.layers.dense(fc, n_classes)
 
     return out
 
@@ -59,7 +60,7 @@ def conv_net(x_dict, n_classes, img_size, dropout, reuse, is_training):
 def model_fn(features, labels, mode, params):
     
     # Build the neural network
-    # Because Dropout have different behavior at training and prediction time, we
+    # Because Dropout has different behavior at training and prediction time, we
     # need to create 2 distinct computation graphs that still share the same weights.
     logits_train = conv_net(features, params['num_classes'], params['img_size'], params['dropout_rate'], reuse=False, is_training=True)
     logits_test = conv_net(features, params['num_classes'], params['img_size'], params['dropout_rate'], reuse=True, is_training=False)
@@ -67,31 +68,28 @@ def model_fn(features, labels, mode, params):
     # Predictions
     pred_classes = tf.argmax(logits_test, axis=1)
     pred_probas = tf.nn.softmax(logits_test)
-    
-    # If prediction mode, early return
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, 
-            predictions=pred_classes,
-            export_outputs={'classes': tf.estimator.export.PredictOutput(pred_classes)}) 
-        
-    # Define loss and optimizer
-    loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits_train, labels=tf.cast(labels, dtype=tf.int32)))
-    optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
-    train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
-    
-    # Evaluate the accuracy of the model
-    acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
 
-    tf.summary.scalar("loss", loss_op)
-    tf.summary.scalar("accuracy", acc_op[0])
-    
-    # TF Estimators requires to return a EstimatorSpec, that specify
-    # the different ops for training, evaluating, ...
-    estim_specs = tf.estimator.EstimatorSpec(
-      mode=mode,
-      predictions=pred_classes,
-      loss=loss_op,
-      train_op=train_op,
-      eval_metric_ops={'accuracy': acc_op})
-    return estim_specs
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        spec = tf.estimator.EstimatorSpec(mode, 
+            predictions=pred_classes,
+            export_outputs={'classes': tf.estimator.export.PredictOutput(pred_classes)})
+    else:
+        # Define loss and optimizer
+        loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=logits_train, labels=tf.cast(labels, dtype=tf.int32)))
+        optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
+        train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
+        
+        # Evaluate the training accuracy for this batch
+        acc = tf.reduce_mean(tf.cast(tf.equal(tf.cast(labels, tf.int64), pred_classes), tf.float32))
+
+        tf.summary.scalar("loss", loss_op)
+        tf.summary.scalar("accuracy", acc)
+        
+        spec = tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=pred_classes,
+            loss=loss_op,
+            train_op=train_op)
+
+    return spec
